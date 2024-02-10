9a7ea1751b514131a98be8c29da9d7b43c1c9bdf
Lorena Rosati <lrosati@outlook.com>
2024-02-10 15:50:47 -0500

    Time: 17 ms (49.05%) | Memory: 11.9 MB (74.13%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        stack = []
+        for i in range(len(s)):
+            if len(stack) == 0 and (s[i] == ')' or s[i] == ']' or s[i] == '}'):
+                return False
+            elif s[i] == ')':
+                if stack[-1] == '(':
+                    stack.pop()
+                else:
+                    return False
+            elif s[i] == ']':
+                if stack[-1] == '[':
+                    stack.pop()
+                else:
+                    return False
+            elif s[i] == '}':
+                if stack[-1] == '{':
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(s[i])
+        return stack == []
