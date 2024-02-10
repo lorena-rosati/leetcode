b1b7368af377ebe549d7ee1733b894202172ba0f
Lorena Rosati <lrosati@outlook.com>
2024-02-10 15:54:44 -0500

    Time: 14 ms (71.08%) | Memory: 11.9 MB (74.13%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -6,23 +6,24 @@ class Solution(object):
         """
         stack = []
         for i in range(len(s)):
-            if len(stack) == 0 and (s[i] == ')' or s[i] == ']' or s[i] == '}'):
+            if len(stack) == 0 and (s[i] in [')',']','}']):
                 return False
-            elif s[i] == ')':
-                if stack[-1] == '(':
-                    stack.pop()
-                else:
-                    return False
-            elif s[i] == ']':
-                if stack[-1] == '[':
-                    stack.pop()
-                else:
-                    return False
-            elif s[i] == '}':
-                if stack[-1] == '{':
-                    stack.pop()
-                else:
-                    return False
+            elif s[i] in [')',']','}']:
+                if s[i] == ')':
+                    if stack[-1] == '(':
+                        stack.pop()
+                    else:
+                        return False
+                elif s[i] == ']':
+                    if stack[-1] == '[':
+                        stack.pop()
+                    else:
+                        return False
+                elif s[i] == '}':
+                    if stack[-1] == '{':
+                        stack.pop()
+                    else:
+                        return False
             else:
                 stack.append(s[i])
         return stack == []
