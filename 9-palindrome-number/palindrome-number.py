4cd54d8471ef8417cfdfc967df13bb6824f14900
Lorena Rosati <lrosati@outlook.com>
2024-02-09 21:11:42 -0500

    Time: 52 ms (27.89%) | Memory: 11.7 MB (78.08%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -4,14 +4,19 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        return self.helper(str(x))
 
-    def helper(self,x):
-        if len(x) == 1:
-            return True
-        elif (len(x) == 2):
-            return True if x[0] == x[1] else False
-        elif x[0] == x[-1]:
-            return self.helper(x[1:len(x)-1]) and True
-        else:
+        if x < 0:
             return False
+        elif x == 0:
+            return True
+        else:
+            temp = x
+            rev = ""
+            while temp != 0:
+                rev += str(temp % 10)
+                temp = temp // 10
+            if str(x) == rev:
+                return True
+            else:
+                return False
+            
