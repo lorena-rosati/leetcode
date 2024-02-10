048ca2911121e2763b855b236d91aba48530c90f
Lorena Rosati <lrosati@outlook.com>
2024-02-09 20:21:35 -0500

    Time: 20 ms (93.29%) | Memory: 11.7 MB (85.84%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -5,17 +5,12 @@ class Solution(object):
         :rtype: int
         """
         dict = {'I': 1, 'V':5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        excep = ['IV', 'IX', 'XL', 'XC', 'CD', 'CM']
 
         sum = 0
 
         for i in range(len(s)):
-            if i < len(s)-1 and s[i] in ['I','X','C']:
-                # s[keys.index(s[i])+1] == s[i+1] or s[keys.index(s[i])+2] == s[i+1]:
-                if s[i:i+2] in excep:
-                    sum -= dict[s[i]]
-                else:
-                    sum += dict[s[i]]
+            if i < len(s)-1 and dict[s[i]] < dict[s[i+1]]:
+                sum -= dict[s[i]]
             else:
                 sum += dict[s[i]]
 
