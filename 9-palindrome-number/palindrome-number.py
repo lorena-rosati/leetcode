5e8a7aa4922b295e4874b5ba6e4e1b652925ddbb
Lorena Rosati <lrosati@outlook.com>
2024-02-09 20:43:56 -0500

    Time: 46 ms (44.55%) | Memory: 11.6 MB (78.08%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0:
+            return False
+        else:
+            return self.helper(str(x))
+
+    def helper(self,x):
+        if len(x) == 1:
+            return True
+        elif (len(x) == 2):
+            return True if x[0] == x[1] else False
+        elif x[0] == x[-1]:
+            return self.helper(x[1:len(x)-1]) and True
+        else:
+            return False
