4d4e55efd5af871560fe9e125037a766aa8ea4eb
Lorena Rosati <lrosati@outlook.com>
2024-02-11 15:19:56 -0500

    Time: 22 ms (42.57%) | Memory: 11.7 MB (74.84%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -13,17 +13,7 @@ class Solution(object):
         head = ListNode()
         temp = head
 
-        if not list1:
-            return list2
-        elif not list2:
-            return list1
-
-        if list1.val < list2.val:
-            head.val = list1.val
-            list1 = list1.next
-        else:
-            head.val = list2.val
-            list2 = list2.next
+        
 
         while list1 and list2:
             if list1.val < list2.val:
@@ -34,10 +24,7 @@ class Solution(object):
                 list2 = list2.next
             temp = temp.next
 
-        if list1: 
-            temp.next = list1
-        elif list2:
-            temp.next = list2
+        temp.next = list1 if list1 else list2
 
-        return head
+        return head.next
                 
