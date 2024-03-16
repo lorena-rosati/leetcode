8012dbb46b8ac334d9abb4937cd34408c9ac3289
Lorena Rosati <lrosati@outlook.com>
2024-03-16 14:41:57 -0400

    Time: 20 ms (45.64%) | Memory: 13.6 MB (78.23%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        temp = head
+        curr = temp
+        prev = None
+        while curr:
+            temp = curr.next
+            curr.next = prev
+            prev = curr
+            curr = temp
+        return prev
+        
+        
