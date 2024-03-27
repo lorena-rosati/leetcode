d8743d0292ea40cbec7ad2f4a922dbf0e127d397
Lorena Rosati <lrosati@outlook.com>
2024-03-26 21:32:59 -0400

    Time: 14 ms (97.14%) | Memory: 11.6 MB (73.23%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.py
@@ -11,7 +11,7 @@ class Solution(object):
         """
 
         curr = head
-        while curr and curr.next:
+        while curr is not None and curr.next is not None:
             if curr.val == curr.next.val:
                 curr.next = curr.next.next
             else:
