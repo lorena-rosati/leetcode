55e185a88a299155c903d9c9a3c3bb06c8fc6a31
Lorena Rosati <lrosati@outlook.com>
2024-05-20 19:46:27 -0400

    Time: 277 ms (76.38%) | Memory: 19.2 MB (52.30%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.py
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -4,17 +4,14 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        pre, post, ans = [1] * len(nums), [1] * len(nums), [1] * len(nums)
+        ans = [1] * len(nums)
         prefix = 1
         for i in range(len(nums)):
-            pre[i] = prefix
+            ans[i] = prefix
             prefix *= nums[i]
         postfix = 1
         for i in range(len(nums)-1, -1, -1):
-            post[i] = postfix 
+            ans[i] *= postfix 
             postfix *= nums[i]
         
-        for i in range(len(nums)):
-            pre[i] *= post[i]
-
-        return pre
+        return ans
