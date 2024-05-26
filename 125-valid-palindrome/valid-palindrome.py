f7cb052b8b952296dc7a17803335b1c86f685287
Lorena Rosati <lrosati@outlook.com>
2024-05-26 17:42:10 -0400

    Time: 54 ms (33.56%) | Memory: 12.1 MB (99.69%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -4,15 +4,18 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        l, r = 0, len(s) -1
+        l, r = 0, len(s) - 1
         while l < r:
-            while not self.isAlphaNum(s[l]) and l < r:
+            if not self.isAlphaNum(s[l]):
                 l += 1
-            while not self.isAlphaNum(s[r]) and l < r:
+                continue
+            if not self.isAlphaNum(s[r]):
                 r -= 1
-            if s[l].lower() != s[r].lower():
+                continue
+            if s[r].lower() == s[l].lower():
+                l, r = l + 1, r - 1
+            else:
                 return False
-            l, r = l + 1, r - 1
         return True
 
     def isAlphaNum(self, c):
