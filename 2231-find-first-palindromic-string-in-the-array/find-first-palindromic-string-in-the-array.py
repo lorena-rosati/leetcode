5d6599f1f57302bd685d4390328ff4c6a65713b2
Lorena Rosati <lrosati@outlook.com>
2024-05-26 19:52:27 -0400

    Time: 71 ms (20.35%) | Memory: 11.7 MB (95.21%) - LeetSync

--- 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.py
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.py
@@ -9,16 +9,9 @@ class Solution(object):
                 return word
         return ""         
 
-    def isAlphaNum(self, c):
-        return (ord("A") <= ord(c) <= ord("Z")) or (ord("a") <= ord(c) <= ord("z")) or (ord("0") <= ord(c) <= ord("9"))
-
     def isPal(self, word):
         l, r = 0, len(word) - 1
         while l < r:
-            while not self.isAlphaNum(word[l]) and l < r:
-                l += 1
-            while not self.isAlphaNum(word[r]) and l < r:
-                r -= 1
             if word[l] != word[r]:
                 return False
             l, r = l + 1, r - 1
