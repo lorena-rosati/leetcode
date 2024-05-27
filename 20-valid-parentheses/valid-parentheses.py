0d5eb24d996e82ed0d716eb3afb40af842eb33b4
Lorena Rosati <lrosati@outlook.com>
2024-05-26 23:13:51 -0400

    Time: 14 ms (64.84%) | Memory: 11.7 MB (56.16%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -4,32 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        if len(s) % 2 != 0:
-            return False
-
         stack = []
-        for c in s:
-            l = len(stack)
-            if c == "(" or c == "{" or c == "[":
-                stack.append(c)
+        for p in s:
+            if p == "(" or p == "{" or p == "[":
+                stack.append(p)
             else:
-                if l == 0:
+                if len(stack) == 0:
                     return False
                 else:
-                    if c == ")":
-                        if stack[-1] == "(":
-                            stack.pop()
-                        else:
-                            return False
-                    elif c == "}":
-                        if stack[-1] == "{":
-                            stack.pop()
-                        else:
-                            return False
-                    elif c == "]":
-                        if stack[-1] == "[":
-                            stack.pop()
-                        else:
-                            return False
-        
+                    if (p == ")" and stack[-1] == "(") or (p == "]" and stack[-1] == "[") or (p == "}" and stack[-1] == "{"):
+                        stack.pop()
+                    else:
+                        return False
         return len(stack) == 0
