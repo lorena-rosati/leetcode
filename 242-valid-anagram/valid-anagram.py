db0a652970b147efa9393e23ed9c8549d0d5bd0e
Lorena Rosati <lrosati@outlook.com>
2024-06-22 11:22:10 -0400

    Time: 39 ms (33.91%) | Memory: 12.7 MB (47.77%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -5,13 +5,4 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if len(s) != len(t):
-            return False
-        hashS, hashT = {}, {}
-        for i in range(len(s)):
-            hashS[s[i]] = 1 + hashS.get(s[i], 0)
-            hashT[t[i]] = 1 + hashT.get(t[i], 0)
-        for j in s:
-            if hashS[j] != hashT.get(j, 0):
-                return False
-        return True
+        return sorted(s) == sorted(t)
