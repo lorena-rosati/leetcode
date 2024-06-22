8228807226b3ae1e5cfd9744e358146755b5526b
Lorena Rosati <lrosati@outlook.com>
2024-06-22 11:51:50 -0400

    Time: 73 ms (56.69%) | Memory: 15.7 MB (64.87%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -4,12 +4,9 @@ class Solution(object):
         :type strs: List[str]
         :rtype: List[List[str]]
         """
-        hashMap = defaultdict(list)
+        d = defaultdict(list)
         for s in strs:
-            count = [0] * 26
-            for c in s:
-                i = ord(c) - ord("a")
-                count[i] += 1
-            hashMap[tuple(count)].append(s)
-        return hashMap.values()
+            ss = ''.join(sorted(s))
+            d[ss].append(s)
         
+        return d.values()
