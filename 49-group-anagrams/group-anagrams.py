3477d765b938b29359160ab3a3af5c950fae8ea0
Lorena Rosati <lrosati@outlook.com>
2024-07-09 17:20:46 -0400

    Time: 85 ms (22.28%) | Memory: 18 MB (6.97%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -11,14 +11,20 @@ class Solution(object):
 
         hashmap = defaultdict(list)
 
-        for s in strs:
+        for s in strs: # runtime -> len(strs)
             key = [0] * 26
-            for c in s:
+            for c in s: # runtime -> len(s)
                 key[ord(c) - ord('a')] += 1
             hashmap[tuple(key)].append(s)
 
         return hashmap.values()
 
+#above code
+        #runtime -> O(len(strs)*avg len of str in strs)
+        #space complexity -> # types of anagrams  *  avg # of variations of each anagram
+            # theres also space for each key, but its length of 26 so O(1)
+
+#old
         #RUNTIME AND MEMORY COMPLEXITY
         # runtime -> O(n) + O(nlgn) + O(n) = O(nlgn)  ... for loop + sorting + convert to tuple
         # space complexity -> 
