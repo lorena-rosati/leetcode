a727dbc25e29a91076bc4476cd0fc00b1191b45c
Lorena Rosati <lrosati@outlook.com>
2024-07-10 11:37:39 -0400

    Time: 80 ms (34.31%) | Memory: 19.1 MB (19.90%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,23 +5,26 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        d = {}
+        arr = [[] for _ in range(len(nums) + 1)]
+        res = []
+
+        # we want to find out how many times each element appears, use hashmap
+        # arr is array where index represents how many times the array of values at the index appears
+        # iterate backwards through arr to find k most freq elements
+
+        hashmap = {}
         for n in nums:
-            d[n] = 1 + d.get(n, 0)
-        
-        a = [[] for _ in range(len(nums)+1)]
-        # index is how many times element shows up, value is an array of the values
-        for key in d:
-            a[d[key]].append(key)
-        
-        s = []
+            hashmap[n] = hashmap.get(n, 0) + 1
+
+        for key in hashmap:
+            arr[hashmap[key]].append(key)
+
         count = 0
-        for i in range(len(a) - 1, 0, -1):
-            if a[i] != []:
-                for j in a[i]:
-                    s.append(j)
-                    count += 1
-                    if count == k:
-                        return s
-        
-        return s
+        for i in range(len(nums), 0, -1):
+            if arr[i] == []:
+                continue
+            for j in arr[i]:
+                res.append(j)
+                count += 1
+                if count == k:
+                    return res
